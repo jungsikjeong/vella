3b0628ac9dff6fdd1f80a5db33212dbc3a86b2e1
jungsikjeong <wndtlr1024@gmail.com>
2021-06-01 16:00:21 +0900

    Add: Login 화면

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,8 +1,148 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+  padding-top: 5rem;
+`;
+
+const Title = styled.div`
+  @media (min-width: 800px) {
+    margin: 2rem;
+  }
+
+  margin: 2rem 0 1rem;
+  min-height: 30px;
+  border-bottom: 0px solid #e8e8e8;
+  h2 {
+    text-align: center;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: #333;
+  }
+`;
+
+const Form = styled.form`
+  border: 0px solid #d7d5d5;
+  margin-bottom: 0.15rem;
+  padding: 1.5rem 0;
+  color: #2e2e2e;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .label {
+    font-size: 0.7rem;
+    padding: 0.6rem 0px 0.4rem;
+  }
+
+  .link {
+    position: relative;
+    padding: 1.25rem 0 0 0px;
+    color: #757575;
+    border-top: 0px solid #000;
+    text-align: center;
+  }
+
+  span {
+    border-bottom: 1px solid #000;
+  }
+
+  ul {
+    display: flex;
+    overflow: hidden;
+    padding: 0.6rem 0;
+    margin: 0;
+    border: 0;
+    text-align: center;
+
+    li:nth-child(1) {
+      padding-left: 0.75rem;
+      padding-right: 0.75rem;
+    }
+
+    li:nth-child(2) {
+      padding-left: 0rem;
+      padding-right: 0.75rem;
+    }
+  }
+`;
+
+const FormContents = styled.div``;
+
+const Input = styled.input`
+  overflow: hidden;
+  display: block;
+  margin: 0 0 0.1rem;
+  border: 1px solid #000;
+  border-radius: 2px;
+  color: #8f8f91;
+  padding: 0.1rem;
+`;
+
+const SLink = styled(Link)`
+  font-size: 0.6rem;
+`;
+
+const Button = styled.button`
+  border: 1px solid #000;
+  background-color: #000;
+  color: #fff;
+  width: 5rem;
+  border: 1px solid #d5d5d5;
+  padding: 0.25rem 0;
+  margin: 0.2rem auto;
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  cursor: pointer;
+  transition: all 0.2s ease;
+
+  :hover {
+    border: 1px solid #000;
+    background: #fff;
+    color: #000;
+  }
+`;
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Title>
+        <h2>login</h2>
+      </Title>
+
+      <Form>
+        <FormContents>
+          <p className='label'>아이디</p>
+          <Input />
+
+          <p className='label'>비밀번호</p>
+          <Input />
+        </FormContents>
+
+        <ul>
+          <li>
+            <SLink to='#'>ID를 잊으셨나요?</SLink>
+          </li>
+          <li>
+            <SLink to='#'>비밀번호를 잊으셨나요?</SLink>
+          </li>
+        </ul>
+
+        <Button>login</Button>
+
+        <p className='link'>
+          <SLink to='/join'>
+            아직 회원이 아니신가요? <span>지금 가입하기</span>
+          </SLink>
+        </p>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
