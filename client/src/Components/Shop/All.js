ddcc91faa2f2e0d13cbab2348352bec804deb7af
jungsikjeong <wndtlr1024@gmail.com>
2021-06-07 15:00:00 +0900

    카테고리 헤더 분류

--- client/src/Components/Shop/All.js
+++ client/src/Components/Shop/All.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Categories from './Common/Categories';
 import Responsive from './Common/Responsiv';
 
 const Container = styled(Responsive)``;
@@ -22,29 +23,6 @@ const ThisCategory = styled.div`
   }
 `;
 
-const Categories = styled.div`
-  ul {
-    display: flex;
-    justify-content: center;
-  }
-  li {
-    padding: 0.4rem 0.75rem 0.4rem 0.75rem;
-
-    .current {
-      border-bottom: 1px solid #333;
-    }
-  }
-`;
-
-const SLink = styled(Link)`
-  font-weight: 600;
-  font-size: 0.6rem;
-  text-align: center;
-  text-transform: uppercase;
-  padding: 0 0.65rem;
-  color: #333;
-`;
-
 const All = () => {
   return (
     <Container>
@@ -52,45 +30,9 @@ const All = () => {
         <ThisCategory>
           <h2>ALL</h2>
         </ThisCategory>
-        <Categories>
-          <ul>
-            <li>
-              <SLink to='/product/all'>
-                <span className='current'>ALL</span>
-              </SLink>
-            </li>
-            <li>
-              <SLink to='/product/top'>
-                <span>Top</span>
-              </SLink>
-            </li>
-            <li>
-              <SLink to='/product/bottom'>
-                <span>Bottom</span>
-              </SLink>
-            </li>
-            <li>
-              <SLink to='/product/dress'>
-                <span>Dress</span>
-              </SLink>
-            </li>
-            <li>
-              <SLink to='/product/outer'>
-                <span>Outer</span>
-              </SLink>
-            </li>
-            <li>
-              <SLink to='/product/promotion'>
-                <span>PROMOTION</span>
-              </SLink>
-            </li>
-            <li>
-              <SLink to='/product/acc'>
-                <span>Acc</span>
-              </SLink>
-            </li>
-          </ul>
-        </Categories>
+
+        {/* 카테고리들 */}
+        <Categories />
       </CategoryWrapper>
     </Container>
   );
---
+++ client/src/Components/Shop/Common/Categories.js
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  ul {
+    display: flex;
+    justify-content: center;
+  }
+  li {
+    padding: 0.4rem 0.75rem 0.4rem 0.75rem;
+
+    .current {
+      border-bottom: 1px solid #333;
+    }
+  }
+`;
+
+const SLink = styled(Link)`
+  font-weight: 600;
+  font-size: 0.6rem;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 0 0.65rem;
+  color: #333;
+`;
+
+const Categories = ({ location: { pathname } }) => {
+  return (
+    <Container>
+      <ul>
+        <li>
+          <SLink to='/product/all'>
+            <span className={pathname === '/product/all' ? 'current' : 0}>
+              ALL
+            </span>
+          </SLink>
+        </li>
+        <li>
+          <SLink to='/product/top'>
+            <span className={pathname === '/product/top' ? 'current' : 0}>
+              Top
+            </span>
+          </SLink>
+        </li>
+        <li>
+          <SLink to='/product/bottom'>
+            <span className={pathname === '/product/bottom' ? 'current' : 0}>
+              Bottom
+            </span>
+          </SLink>
+        </li>
+        <li>
+          <SLink to='/product/dress'>
+            <span className={pathname === '/product/dress' ? 'current' : 0}>
+              Dress
+            </span>
+          </SLink>
+        </li>
+        <li>
+          <SLink to='/product/outer'>
+            <span className={pathname === '/product/outer' ? 'current' : 0}>
+              Outer
+            </span>
+          </SLink>
+        </li>
+        <li>
+          <SLink to='/product/promotion'>
+            <span className={pathname === '/product/promotion' ? 'current' : 0}>
+              PROMOTION
+            </span>
+          </SLink>
+        </li>
+        <li>
+          <SLink to='/product/acc'>
+            <span className={pathname === '/product/acc' ? 'current' : 0}>
+              Acc
+            </span>
+          </SLink>
+        </li>
+      </ul>
+    </Container>
+  );
+};
+
+export default withRouter(Categories);
