d0c824f657d338f6f413085cd60e43f956a33aca
jungsikjeong <wndtlr1024@gmail.com>
2021-06-10 01:33:43 +0900

    Add: 메인 관리자 페이지

--- client/src/Components/Admin/Sections/AdminHome/AdminHome.js
+++ client/src/Components/Admin/Sections/AdminHome/AdminHome.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Table } from 'antd';
+
 import Responsive from '../../../Common/Responsive';
 
 const Container = styled(Responsive)``;
 
+const columns = [
+  {
+    title: 'Title',
+    dataIndex: 'title',
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+  },
+  {
+    title: 'price',
+    dataIndex: 'price',
+  },
+  {
+    title: 'Images',
+    dataIndex: 'images',
+  },
+];
+
+const data = [];
+
+for (let i = 0; i < 46; i++) {
+  data.push({
+    key: i,
+    title: `제목 ${i}`,
+    description: 32,
+    price: `${i}원`,
+    images: `이미지 ${i}`,
+  });
+}
+
 const AdminHome = () => {
-  return <Container>AdminHome</Container>;
+  const [SelectedRowKeys, setSelectedRowKeys] = useState([]);
+
+  const onSelectChange = (selectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    setSelectedRowKeys({ selectedRowKeys });
+  };
+
+  const rowSelection = {
+    SelectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  return (
+    <Container>
+      <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
+    </Container>
+  );
 };
 
 export default AdminHome;
